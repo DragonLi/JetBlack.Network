47bc4506fcbcda07ef321c4bbb31c3096806d580
dragon4147 <dragonlyj@gmail.com>
2017-12-05 10:45:28 +0800

    prepare server protocol

--- RxSocket.EchoServer/EchoProtocolServerProgram.cs
+++ RxSocket.EchoServer/EchoProtocolServerProgram.cs
@@ -7,6 +7,24 @@ namespace RxSocket.EchoServer
     {
         public static void Main(string[] args)
         {
+            /*var endpoint = ProgramArgs.Parse(args, new[] { "127.0.0.1:9211" }).EndPoint;
+
+            var cts = new CancellationTokenSource();
+
+            endpoint.ToListenerObservable(10)
+                .ObserveOn(TaskPoolScheduler.Default)
+                .Subscribe(
+                    client =>
+                        client.ToClientObservable(1024, SocketFlags.None)
+                            .Subscribe(client.ToClientObserver(1024, SocketFlags.None), cts.Token),
+                    error => Console.WriteLine("Error: " + error.Message),
+                    () => Console.WriteLine("OnCompleted"),
+                    cts.Token);
+
+            Console.WriteLine("Press <ENTER> to quit");
+            Console.ReadLine();
+
+            cts.Cancel();*/
         }
     }
 }
