0f64d2a3de947a300affd9f8a92f75a72b641492
DragonLi <dragonlyj@gmail.com>
2017-12-05 18:42:14 +0800

    brain device frame decoding/encoding protocol implementation

---
+++ BrainDeviceProtocol/Utility.cs
@@ -0,0 +1,11 @@
+﻿namespace BrainDeviceProtocol
+{
+    public static class Utility
+    {
+        public static string Show(this byte data)
+        {
+            //TODO
+            return "";
+        }
+    }
+}
